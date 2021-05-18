5149dc0778ecfe7494a24457d8631e3d790532f5
Riya Acharya <ria.acharya07@gmail.com>
2021-05-18 17:13:45 +0545

    custom widget created

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './buttons.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -8,28 +10,67 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: SafeArea(
-      child: Scaffold(
-        body: Column(
-          children: [
-            Expanded(
-              child: Container(
-                color: Colors.purple,
-              ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> buttons = [
+    'C',
+    'DEL',
+    '%',
+    '/',
+    '9',
+    '8',
+    '7',
+    'x',
+    '6',
+    '5',
+    '4',
+    '+',
+    '3',
+    '2',
+    '1',
+    '-',
+    '0',
+    '.',
+    'ANS',
+    '=',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              child: GridView.builder(
+                  itemCount: buttons.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4),
+                  itemBuilder: (BuildContext context, int index) {
+                    return MyButton(
+                      buttonText: buttons[index],
+                      color: Colors.deepPurple,
+                      textColor: Colors.white,
+                    );
+                  }),
             ),
-            Expanded(
-              child: Container(
-                color: Colors.pink,
-              ),
-            ),
-            Expanded(
-              child: Container(
-                color: Colors.green,
-              ),
-            ),
-          ],
-        ),
+          )
+        ],
       ),
-    ));
+    );
   }
 }
